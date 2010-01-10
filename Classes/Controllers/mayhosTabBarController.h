733e69ca920f1fe58179451f8a4ccd20cbf78907
Can Berk Güder <cbguder@gmail.com>
2010-01-10 22:42:22 +0200

    Subclass UITabBarController to support rotation

---
+++ Classes/Controllers/mayhosTabBarController.h
@@ -0,0 +1,14 @@
+//
+//  mayhosTabBarController.h
+//  mayhos
+//
+//  Created by Can Berk Güder on 6/1/2010.
+//  Copyright 2010 Can Berk Güder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface mayhosTabBarController : UITabBarController {
+}
+
+@end
---
+++ Classes/Controllers/mayhosTabBarController.m
@@ -0,0 +1,17 @@
+//
+//  mayhosTabBarController.m
+//  mayhos
+//
+//  Created by Can Berk Güder on 6/1/2010.
+//  Copyright 2010 Can Berk Güder. All rights reserved.
+//
+
+#import "mayhosTabBarController.h"
+
+@implementation mayhosTabBarController
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+	return YES;
+}
+
+@end
