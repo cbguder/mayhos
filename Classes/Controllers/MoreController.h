8969e3c9968c48b1b0ba04d6b9fec5a34358c5d1
Can Berk Güder <cbguder@gmail.com>
2010-04-30 00:42:05 +0300

    Move *a lot* of functionality to MoreController.

---
+++ Classes/Controllers/MoreController.h
@@ -0,0 +1,14 @@
+//
+//  MoreController.h
+//  mayhos
+//
+//  Created by Can Berk Güder on 29/4/2010.
+//  Copyright 2010 Can Berk Güder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MoreController : UITableViewController {
+}
+
+@end
