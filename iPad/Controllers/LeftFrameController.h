7c2fa99003cf8d06f66f1af48cb9e4e81c195125
Can Berk Güder <cbguder@gmail.com>
2010-05-04 04:10:54 +0300

    Add nonfunctional LeftFrameController.

---
+++ iPad/Controllers/LeftFrameController.h
@@ -0,0 +1,14 @@
+//
+//  LeftFrameController.h
+//  mayhos
+//
+//  Created by Can Berk Güder on 4/5/2010.
+//  Copyright 2010 Can Berk Güder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LeftFrameController : UITableViewController {
+}
+
+@end
---
+++ iPad/Controllers/LeftFrameController.m
@@ -0,0 +1,19 @@
+//
+//  LeftFrameController.m
+//  mayhos
+//
+//  Created by Can Berk Güder on 4/5/2010.
+//  Copyright 2010 Can Berk Güder. All rights reserved.
+//
+
+#import "LeftFrameController.h"
+
+@implementation LeftFrameController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	self.clearsSelectionOnViewWillAppear = NO;
+	self.contentSizeForViewInPopover = CGSizeMake(320.0, 600.0);
+}
+
+@end
