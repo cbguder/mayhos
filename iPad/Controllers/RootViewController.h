d1d8d325ad7494b3f4ebbbbea842ddc226da5a72
Can Berk Güder <cbguder@gmail.com>
2010-07-23 15:44:49 +0300

    Add search to RootViewController (and let MW adapt).

--- iPad/Controllers/RootViewController.h
+++ iPad/Controllers/RootViewController.h
@@ -8,7 +8,8 @@
 
 #import <UIKit/UIKit.h>
 
-@interface RootViewController : UITableViewController {
+@interface RootViewController : UITableViewController <UISearchBarDelegate, UISearchDisplayDelegate> {
+	NSMutableArray *matches;
 }
 
 @end
--- iPad/Controllers/RootViewController.m
+++ iPad/Controllers/RootViewController.m
@@ -7,9 +7,10 @@
 //
 
 #import "RootViewController.h"
-#import "LeftFrameController.h"
+#import "FavoritedLeftFrameController.h"
 #import "RightFrameController.h"
 #import "FavoritesController_Pad.h"
+#import "HistoryManager.h"
 
 enum {
 	kRowToday,
@@ -23,7 +24,15 @@ enum {
 	kRowLast
 };
 
+@interface RootViewController ()
+@property (nonatomic,retain) NSMutableArray *matches;
+- (void)filter:(NSString *)query;
+- (void)search:(NSString *)query;
+@end
+
 @implementation RootViewController
+
+@synthesize matches;
 
 #pragma mark -
 #pragma mark View lifecycle
@@ -32,6 +41,11 @@ enum {
 	[super viewDidLoad];
 	self.title = @"mayhoş";
 	self.clearsSelectionOnViewWillAppear = NO;
+
+	// TODO: Private API
+	if([self.searchDisplayController.searchBar respondsToSelector:@selector(setCombinesLandscapeBars:)]) {
+		[self.searchDisplayController.searchBar setCombinesLandscapeBars:NO];
+	}
 }
 
 - (void)viewWillAppear:(BOOL)animated {
@@ -46,6 +60,11 @@ enum {
 	}
 }
 
+- (void)viewWillDisappear:(BOOL)animated {
+	[super viewWillDisappear:animated];
+	[self.searchDisplayController setActive:NO animated:animated];
+}
+
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
 	return YES;
 }
@@ -53,7 +72,11 @@ enum {
 #pragma mark -
 #pragma mark Table view data source
 
-- (NSInteger)tableView:(UITableView *)aTableView numberOfRowsInSection:(NSInteger)section {
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+	if(tableView == self.searchDisplayController.searchResultsTableView) {
+		return [matches count];
+	}
+
 	return kRowLast;
 }
 
@@ -63,6 +86,11 @@ enum {
 	UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
 	if(cell == nil) {
 		cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] autorelease];
+	}
+
+	if(tableView == self.searchDisplayController.searchResultsTableView) {
+		cell.textLabel.text = [matches objectAtIndex:indexPath.row];
+		return cell;
 	}
 
 	if(indexPath.row == kRowFeatured || indexPath.row == kRowFAQ) {
@@ -96,10 +124,27 @@ enum {
 	return cell;
 }
 
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+	return (tableView == self.searchDisplayController.searchResultsTableView);
+}
+
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+	if(editingStyle == UITableViewCellEditingStyleDelete) {
+		[[HistoryManager sharedManager] removeString:[self.matches objectAtIndex:indexPath.row]];
+		[self.matches removeObjectAtIndex:indexPath.row];
+		[tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:YES];
+	}
+}
+
 #pragma mark -
 #pragma mark Table view delegate
 
-- (void)tableView:(UITableView *)aTableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+	if(tableView == self.searchDisplayController.searchResultsTableView) {
+		[self search:[matches objectAtIndex:indexPath.row]];
+		return;
+	}
+
  	if(indexPath.row == kRowFavorites) {
 		FavoritesController_Pad *favoritesController = [[FavoritesController_Pad alloc] init];
 		favoritesController.title = @"favorites";
@@ -139,6 +184,68 @@ enum {
 			leftFrameController.title = @"rastgele";
 			leftFrameController.URL = [API randomURL];
 		}
+
+		[self.navigationController pushViewController:leftFrameController animated:YES];
+		[leftFrameController release];
+	}
+}
+
+#pragma mark -
+#pragma mark Search bar delegate
+
+- (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar {
+	[self search:searchBar.text];
+}
+
+#pragma mark -
+#pragma mark Search display controller delegate
+
+- (BOOL)searchDisplayController:(UISearchDisplayController *)controller shouldReloadTableForSearchString:(NSString *)searchString {
+	[self filter:searchString];
+	return YES;
+}
+
+- (void)searchDisplayController:(UISearchDisplayController *)controller willUnloadSearchResultsTableView:(UITableView *)tableView {
+	self.matches = nil;
+}
+
+- (void)searchDisplayController:(UISearchDisplayController *)controller didHideSearchResultsTableView:(UITableView *)tableView {
+	self.matches = nil;
+}
+
+#pragma mark -
+#pragma mark Memory management
+
+- (void)dealloc {
+	[matches release];
+	[super dealloc];
+}
+
+#pragma mark -
+#pragma mark Search
+
+- (void)filter:(NSString *)query {
+	if([query isEqualToString:@""]) {
+		self.matches = nil;
+		return;
+	}
+
+	NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF like[c] %@", [NSString stringWithFormat:@"*%@*", query]];
+	self.matches = [NSMutableArray arrayWithArray:[[HistoryManager sharedManager].history allObjects]];
+	[matches filterUsingPredicate:predicate];
+	[matches sortUsingSelector:@selector(localizedCaseInsensitiveCompare:)];
+}
+
+- (void)search:(NSString *)query {
+	[[HistoryManager sharedManager] addString:query];
+
+	if(self.searchDisplayController.searchBar.selectedScopeButtonIndex == 0) {
+		[self.searchDisplayController.searchBar resignFirstResponder];
+		UIAppDelegatePad.rightFrameController.eksiTitle = [EksiTitle titleWithTitle:query];
+	} else {
+		FavoritedLeftFrameController *leftFrameController = [[FavoritedLeftFrameController alloc] init];
+		leftFrameController.URL = [API URLForSearchQuery:query];
+		leftFrameController.title = query;
 
 		[self.navigationController pushViewController:leftFrameController animated:YES];
 		[leftFrameController release];
